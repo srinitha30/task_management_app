11fa417e9b985982cff7bf944054a6903eba0f97
Srinitha30 <Srinitha1106@gmail.com>
2026-08-17 19:44:14 +0530

    Fix Task model import

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const Task = require("../models/Task");
+const Task = require("../models/task");
 const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
